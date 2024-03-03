bc586f6be409012dfbc70335f0fc918262e338bd
Lizernz <154061095+Lizernz@users.noreply.github.com>
2024-03-03 20:10:41 +0000

    Improved the User experience

--- index.js
+++ index.js
@@ -9,24 +9,22 @@ function displayPoem(response){
     }
 
 
-
-
-
-
-
 function generatePoem(event) {
     event.preventDefault();
 
 let instructionsInput = document.querySelector("#user-instructions");
-    let apiKey = "1de043300tfb174cf1a30ef403a9aobc";
-    let context = 
-    "You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with `SheCodes AI` inside a <strong> element at the end of the poem";
+let apiKey = "1de043300tfb174cf1a30ef403a9aobc";
+let context = 
+    "You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with `SheCodes AI` in bold inside a <strong> element at the end of the poem";
 
 let prompt = `User instructions: Generate a poem about seduction ${instructionsInput.value}`;
 
-    let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
     //Make a call to the API
     //display the generated poem
+let poemElement = document.querySelector("#poem");
+poemElement.classList.remove("hidden");
+poemElement.innerHTML = `<div class="generating">🕒 Generating a poem about ${instructionsInput.value}</div>`;
 
     console.log("Generating poem");
     console.log(`Prompt: ${prompt}`);
